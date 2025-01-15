6268da4f15f6f86e1a4a4ec7d3f33df78aac8b0d
Marko Šeparović <maseparov@vsite.hr>
2025-01-15 14:13:43 +0100

    morao sam promijeniti app.cpp da bi mi build proradio i test pokazao sve tocno. ako ima jos nekih stvari koje moram promijeniti, ucinit cu to

--- app/app.cpp
+++ app/app.cpp
@@ -1,51 +1,59 @@
 #include "app.h"
+#include <unordered_set>
+#include <sstream>
+#include <algorithm>
 
 namespace vsite::oop::v9
 {
-	void fill_vector(std::vector<int>& v, int len) {
-		for (int i = 0; i < len; i++) {
-			v.push_back(i * i);
-		}
-	}
-	void remove_element(std::vector<int>& v, int n) {
-		v.erase(v.begin() + n);
-	}
-	void input_element(std::vector<std::string>& s, int n, std::string str) {
-		s.insert(s.begin() + n, str);
-	}
-	int list_nth_element(std::list<int>& l, int n) {
-		std::list<int>::iterator i = l.begin();
-		std::advance(i, n);
-		return *i;
-	}
-	void list_sort_desc(std::list<int>& l) {
-		l.sort(std::greater<int>());
-	}
-	unsigned int unique_numbers(std::istream& is) {
-		std::unordered_set<int> nums;
-		int v;
-		while (is >> v) {
-			nums.insert(v);
-		}
-		return nums.size();
-	}
+    void fill_vector(std::vector<int>& v, int len) {
+        for (int i = 0; i < len; i++) {
+            v.push_back(i * i);
+        }
+    }
 
-	word_frequency::word_frequency(std::stringstream& s) {
-		std::string word;
-		while (s >> word) {
-			for (char& c : word) {
-				c = std::tolower(c);
-			}
-			map[word]++;
-		}
-	}
+    void remove_element(std::vector<int>& v, int n) {
+        v.erase(v.begin() + n);
+    }
 
-	unsigned int word_frequency::count() const {
-		return map.size();
-	}
+    void input_element(std::vector<std::string>& s, int n, std::string str) {
+        s.insert(s.begin() + n, str);
+    }
 
-	unsigned int word_frequency::frequency(std::string s) const {
-		auto it = map.find(s);
-		return (it != map.end()) ? it->second : 0;
-	}
+    int list_nth_element(const std::list<int>& l, int n) { // const&
+        auto it = l.begin();
+        std::advance(it, n);
+        return *it;
+    }
+
+    void list_sort_desc(std::list<int>& l) {
+        l.sort(std::greater<int>());
+    }
+
+    unsigned int unique_numbers(std::istream& is) {
+        std::unordered_set<int> nums;
+        int v;
+        while (is >> v) {
+            nums.insert(v);
+        }
+        return nums.size();
+    }
+
+    word_frequency::word_frequency(std::istream& is) { // std::istream&
+        std::string word;
+        while (is >> word) {
+            for (char& c : word) {
+                c = std::tolower(c);
+            }
+            map[word]++;
+        }
+    }
+
+    unsigned int word_frequency::count() const {
+        return map.size();
+    }
+
+    unsigned int word_frequency::frequency(std::string s) const {
+        auto it = map.find(s);
+        return (it != map.end()) ? it->second : 0;
+    }
 }
